9701ddb58e0f31b0c239510072748b6203f8d1c2
choudharymahipal <mschoudhary81@gmail.com>
2025-04-07 22:23:36 +0530

    first commit

---
+++ JWTAuthentication_DotNetCore/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace JWTAuthentication_DotNetCore.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
